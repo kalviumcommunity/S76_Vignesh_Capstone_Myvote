4cc52ec23abac9c967dcfe7070fb00467d5793fa
Vigneshkalvium <vipvignesh.dr.2244@gmail.com>
2025-04-26 09:30:45 +0530

    jwt authentication implemented

---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const authenticateUser = (req, res, next) => {
+    const token = req.cookies.token; 
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
+     
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded; 
+      next();
+    } catch (err) {
+      return res.status(403).json({ message: 'Forbidden' });
+    }
+  };
+  
+  
+ module.exports = authenticateUser;
+  
